b411740e0f439d5a5d257f74f74945f86585684a
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-10-21 05:14:51 -0500

    Bump version, add support for win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,28 +4,37 @@ if "%ARCH%" == "32" (
   set ARCH=x64
 )
 
+:: See https://github.com/conda-forge/staged-recipes/pull/194#issuecomment-203577297
+:: Nasty workaround. Need to move a more current msbuild into PATH.  The one on
+:: AppVeyor barfs on the solution. This one comes from the Win7 SDK (.net 4.0),
+:: and is known to work.
+if %VS_MAJOR% == 9 (
+    COPY C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\
+    set "PATH=%CD%;%PATH%"
+)
+
 if "%VS_YEAR%" == "2008" (
   xcopy /s /y /i %RECIPE_DIR%\vs2008 %SRC_DIR%\builds\msvc\vs%VS_YEAR%
   copy %LIBRARY_INC%\stdint.h %SRC_DIR%\builds\msvc\
   copy %LIBRARY_INC%\inttypes.h %SRC_DIR%\builds\msvc\
-  set ARTIFACTS_DIR=%SRC_DIR%\bin\x64\Release\v90\
-  cd %SRC_DIR%\builds\msvc\vs%VS_YEAR%\dynamic
-  msbuild libsodium.sln  /p:Configuration=Release /p:Platform=%ARCH%
+  cd /d %SRC_DIR%\builds\msvc\vs%VS_YEAR%\dynamic
+  msbuild libsodium.sln /p:Configuration=Release /p:Platform=%ARCH%
   if errorlevel 1 exit 1
-  cd %SRC_DIR%\builds\msvc\vs%VS_YEAR%\static
-  msbuild libsodium.sln  /p:Configuration=Release /p:Platform=%ARCH%
+  cd /d %SRC_DIR%\builds\msvc\vs%VS_YEAR%\static
+  msbuild libsodium.sln /p:Configuration=Release /p:Platform=%ARCH%
   if errorlevel 1 exit 1
+  set ARTIFACTS_DIR=%SRC_DIR%\bin\%ARCH%\Release\v90\
 ) else (
-  cd %SRC_DIR%\builds\msvc\vs%VS_YEAR%\
+  cd /d %SRC_DIR%\builds\msvc\vs%VS_YEAR%\
   msbuild libsodium.sln /p:Configuration=DynRelease /p:Platform=%ARCH%
   if errorlevel 1 exit 1
   msbuild libsodium.sln /p:Configuration=StaticRelease /p:Platform=%ARCH%
   if errorlevel 1 exit 1
-  set ARTIFACTS_DIR=%SRC_DIR%\bin\x64\Release\v140\
+  set ARTIFACTS_DIR=%SRC_DIR%\bin\%ARCH%\Release\v140\
 )
 
 if "%VS_YEAR%" == "2010" (
-  set ARTIFACTS_DIR=%SRC_DIR%\bin\x64\Release\v100\
+  set ARTIFACTS_DIR=%SRC_DIR%\bin\%ARCH%\Release\v100\
 )
 
 move %ARTIFACTS_DIR%\dynamic\libsodium.dll %LIBRARY_BIN%
